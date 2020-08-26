d38a0af5b43e6adb57fafbf292a86b103e1c6055
Alexandre <alexandre.omph@gmail.com>
2020-08-26 03:07:47 -0300

    feat: invalidating cache

--- server/src/modules/users/services/CreateUserService.ts
+++ server/src/modules/users/services/CreateUserService.ts
@@ -6,6 +6,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import AppError from '@shared/errors/AppError';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   name: string;
@@ -22,6 +23,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -38,6 +42,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list')
 
     return user;
   }
--- server/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ server/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -23,5 +23,15 @@ export default class RedisCacheProvider implements ICacheProvider {
     return parsedData
   }
   public async invalidate(key: string): Promise<void> { }
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`)
+    const pipeline = await this.client.pipeline()
+
+    keys.forEach(key => {
+      pipeline.del(key)
+    })
+
+    await pipeline.exec();
+  }
 
 }
