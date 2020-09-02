b0e46109e637a068e6311a11a61d1cdc7284597a
Alexandre <alexandre.omph@gmail.com>
2020-09-02 20:13:29 -0300

    feat: change user avatar

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -22,6 +22,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignCredential): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -62,8 +63,16 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback((user: User) => {
+    localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+    setData({
+      token: data.token,
+      user
+    })
+  }, [setData, data.token])
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, updateUser }}>
       {children}
     </AuthContext.Provider>
   );
--- web/src/pages/Profile/index.tsx
+++ web/src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, ChangeEvent } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Form } from '@unform/web';
@@ -26,7 +26,7 @@ const Profile: React.FC = () => {
   const { addToast } = useToast();
   const history = useHistory();
 
-  const { user } = useAuth()
+  const { user, updateUser } = useAuth()
 
   const handleSubmit = useCallback(async (data: ProfileFormData) => {
     try {
@@ -67,6 +67,21 @@ const Profile: React.FC = () => {
     }
   }, []);
 
+  const handleAvatarChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+
+    if (e.target.files) {
+      const data = new FormData()
+      data.append('avatar', e.target.files[0])
+      api.patch('/users/avatar', data).then(response => {
+        updateUser(response.data)
+        addToast({
+          type: 'success',
+          title: 'Avatar atualizado!'
+        })
+      })
+    }
+  }, [addToast])
+
   return (
     <Container>
       <header>
@@ -84,9 +99,10 @@ const Profile: React.FC = () => {
         }} onSubmit={handleSubmit}>
           <AvatarInput>
             <img src={user.avatar_url} alt={user.name} />
-            <button type="button">
+            <label htmlFor="avatar">
               <FiCamera></FiCamera>
-            </button>
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
           </AvatarInput>
 
           <h1>Meu perfil</h1>
