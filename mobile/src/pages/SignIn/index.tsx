906835da9fc253b74c3511526aec34debc309f4d
Alexandre <alexandre.omph@gmail.com>
2020-09-14 17:47:42 -0300

    feat: input & button components

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Image } from 'react-native'
-import { Container, Title } from './styles';
-
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import logo from '../../assets/logo.png'
 
+import { Container, Title } from './styles';
 const SignIn: React.FC = () => {
   return (<Container>
     <Image source={logo}></Image>
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="Email"></Input>
+    <Input name="password" icon="lock" placeholder="Senha"></Input>
+
+    <Button onPress={() => { }}>Entrar</Button>
+
   </Container>)
 }
 
